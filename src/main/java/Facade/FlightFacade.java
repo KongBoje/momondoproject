1429a9deb77d9e3ba5b43ff081519891849633b7
Michael Veilis <Michael Veilis>
2016-11-22 14:01:36 +0100

    Har lavet noget funktion i Facade klassen

--- src/main/java/Facade/FlightFacade.java
+++ src/main/java/Facade/FlightFacade.java
@@ -15,7 +15,7 @@ import javax.persistence.Persistence;
 
 
 public class FlightFacade implements IFlightFacade {
-    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("smpu");
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("mmp");
     private static EntityManager em = emf.createEntityManager();
     
     public FlightFacade() {
@@ -23,23 +23,52 @@ public class FlightFacade implements IFlightFacade {
     }
 
     @Override
-    public Flight getFlight(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Flight getFlight(Long id) {
+        Flight F = null;
+        try {
+            em.getTransaction().begin();
+            F = em.find(Flight.class, id);
+        } finally {
+            em.close();
+        }
+        em.close();
+        return F;
+        
     }
 
     @Override
     public Flight addFlight(Flight flight) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Flight f = flight;
+            try {
+                em.getTransaction().begin();
+                em.persist(f);
+                em.getTransaction().commit();
+            } finally {
+                em.close();
+            }
+        return f;
     }
 
     @Override
-    public Flight deleteFlight(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Boolean deleteFlight(Long id) {
+        Flight f = null;
+        try {
+            em.getTransaction().begin();
+            f = em.find(Flight.class, id);
+            em.remove(f);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+        return true;
     }
 
     @Override
     public List<Flight> getAllFlights() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Flight> result = em.createQuery("SELECT e FROM Flight e").getResultList();
+
+        em.close();
+        return result;
     }
 
     @Override
