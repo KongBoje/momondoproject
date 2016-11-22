cce5361c3e70275ed9744c5afde24e79884cef97
Lasse Hansen <Lasse Hansen>
2016-11-22 11:46:31 +0100

    Added generic exception mapper

--- src/main/java/Rest/ApplicationConfig.java
+++ src/main/java/Rest/ApplicationConfig.java
@@ -30,6 +30,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(Rest.ExceptionResource.class);
+        resources.add(RestException.GenericExceptionMapper.class);
     }
     
 }
