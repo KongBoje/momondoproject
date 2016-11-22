9bbed22d0460b2385d705d896073987de791b139
Lasse Hansen <Lasse Hansen>
2016-11-22 12:23:33 +0100

    Added a FlightExceptionMapper

---
+++ src/main/java/RestException/FlightExceptionMapper.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RestException;
+
+import Exceptions.FlightException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author Lasse
+ */
+@Provider
+public class FlightExceptionMapper implements ExceptionMapper<FlightException> {
+
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    @Context
+    ServletContext context;
+
+    @Override
+    public Response toResponse(FlightException ex) {
+        JsonObject error = new JsonObject();
+        JsonObject errorDetail = new JsonObject();
+
+        errorDetail.addProperty("httpError", ex.httpError);
+        errorDetail.addProperty("errorCode", ex.errorCode);
+        errorDetail.addProperty("message", ex.message);
+        
+        error.add("error", errorDetail);
+        
+        return Response.status(ex.errorCode).entity(gson.toJson(error)).type(MediaType.APPLICATION_JSON).build();
+        
+    }
+}
---
+++ src/main/java/RestException/NotFoundExceptionMapper.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RestException;
+
+/**
+ *
+ * @author Lasse
+ */
+import javax.ws.rs.NotFoundException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
+
+  private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+  @Context
+  ServletContext context;
+
+  @Override
+  public Response toResponse(NotFoundException ex) {
+    JsonObject error = new JsonObject();
+    JsonObject errorDetail = new JsonObject();
+    int statusCode = ex.getResponse().getStatus();
+    errorDetail.addProperty("code", statusCode);
+    errorDetail.addProperty("message", "The requested resource was not found on our server");
+    error.add("error", errorDetail);
+    return Response.status(statusCode).entity(gson.toJson(error)).type(MediaType.APPLICATION_JSON).build();
+  }
+}
