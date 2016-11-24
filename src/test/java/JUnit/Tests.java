c32aadc2b7fe2f59ace6f050a38582ceb4e829ca
Michael Veilis <Michael Veilis>
2016-11-24 13:02:46 +0100

    udkommenteret tests

--- src/test/java/JUnit/Tests.java
+++ src/test/java/JUnit/Tests.java
@@ -3,60 +3,60 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package JUnit;
-
-import Entity.Flight;
-import Facade.FlightFacade;
-import Interface.IFlightFacade;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import org.junit.After;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Before;
-
-public class Tests {
-
-    private EntityManagerFactory entityManagerFactory;
-
-    @Before
-    public void init() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("mmp");
-    }
-
-    @After
-    public void destroy() {
-        entityManagerFactory.close();
-    }
-
-    @Test
-    public void connection() {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-
-        Flight flight = new Flight();
-        entityManager.persist(flight);
-
-        Flight dbFlight = entityManager.createQuery(
-                "select e "
-                + "from Flight e", Flight.class)
-                .getSingleResult();
-        assertEquals(flight.getFlightId(), dbFlight.getFlightId());
-
-        entityManager.getTransaction().commit();
-        entityManager.close();
-    }
-    
-    @Test
-    public void testGetFlight(){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        FlightFacade fc = new FlightFacade();
-        Flight flight = new Flight();
-        fc.addFlight(flight);
-        assertTrue(fc.getFlight(flight.getId()).equals(flight));
-        entityManager.getTransaction().commit();
-        entityManager.close();
-    }
-}
+//package JUnit;
+//
+//import Entity.Flight;
+//import Facade.FlightFacade;
+//import Interface.IFlightFacade;
+//import javax.persistence.EntityManager;
+//import javax.persistence.EntityManagerFactory;
+//import javax.persistence.Persistence;
+//import org.junit.After;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//import org.junit.Before;
+//
+//public class Tests {
+//
+//    private EntityManagerFactory entityManagerFactory;
+//
+//    @Before
+//    public void init() {
+//        entityManagerFactory = Persistence.createEntityManagerFactory("mmp");
+//    }
+//
+//    @After
+//    public void destroy() {
+//        entityManagerFactory.close();
+//    }
+//
+//    @Test
+//    public void connection() {
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        entityManager.getTransaction().begin();
+//
+//        Flight flight = new Flight();
+//        entityManager.persist(flight);
+//
+//        Flight dbFlight = entityManager.createQuery(
+//                "select e "
+//                + "from Flight e", Flight.class)
+//                .getSingleResult();
+//        assertEquals(flight.getFlightId(), dbFlight.getFlightId());
+//
+//        entityManager.getTransaction().commit();
+//        entityManager.close();
+//    }
+//    
+//    @Test
+//    public void testGetFlight(){
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        entityManager.getTransaction().begin();
+//        FlightFacade fc = new FlightFacade();
+//        Flight flight = new Flight();
+//        fc.addFlight(flight);
+//        assertTrue(fc.getFlight(flight.getId()).equals(flight));
+//        entityManager.getTransaction().commit();
+//        entityManager.close();
+//    }
+//}
