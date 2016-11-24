2eefd5920391a2d5bb5a385d6f18cf9ffd720568
Lasse Hansen <Lasse Hansen>
2016-11-24 13:00:39 +0100

    Angular interaction with rest API (can now fetch sample data of Lars

--- src/main/webapp/js/myscript.js
+++ src/main/webapp/js/myscript.js
@@ -4,23 +4,78 @@
  * and open the template in the editor.
  */
 
-
 var app = angular.module("myApp", ["ngRoute"]);
+
+app.factory("dataContainer", function () {
+    var factory = {};
+
+    var myresults = null;
+
+    factory.set = function (data) {
+        myresults = data;
+    }
+
+    factory.get = function () {
+        return myresults;
+    }
+
+    return factory;
+})
 
 app.config(function ($routeProvider) {
     $routeProvider.
             when("/search", {
                 templateUrl: "search_refactor.html",
-        controller: "searchCtrl"
+                controller: "searchCtrl"
             }).when("/results", {
-        templateUrl: "results_refactor.html"
+        templateUrl: "results_refactor.html",
+        controller: "searchCtrl"
     }).otherwise({
         redirectTo: "/search"
     });
 });
 
-app.controller("searchCtrl", ["$scope", function($scope) {
-    $scope.searchFunc = function() {
-        alert("clicked");
-    }
-}])
+app.controller("searchCtrl", ["$scope", "$http", "dataContainer", "$location", function ($scope, $http, dataContainer, $location) {
+        $scope.results = dataContainer.get();
+        $scope.searchFunc = function () {
+
+            if ($scope.fromIATA === undefined) {
+                alert("Fill in a from airpor");
+                return;
+            }
+            if ($scope.onDate === undefined) {
+                alert("Fill in a date");
+                return;
+            }
+            if ($scope.passengerCount === undefined) {
+                alert("Fill in passenger count");
+                return;
+            }
+            var passengers = ($scope.passengerCount).substring(0, 1);
+            if ($scope.toIATA === undefined) {
+                //console.log("To was not set, so just get from");
+
+                $http({
+                    method: "GET",
+                    url: "api/flights/" + $scope.fromIATA + "/" + $scope.onDate + "T00:00:00.000Z" + "/" + passengers
+                }).then(function callback(response) {
+                    dataContainer.set(response.data);
+                    $scope.results = dataContainer.get();
+                    $location.path("/results");
+                });
+
+            } else {
+                //console.log("To: " + $scope.toIATA);
+                $http({
+                    method: "GET",
+                    url: "api/flights/" + $scope.fromIATA + "/" + $scope.toIATA + "/" + $scope.onDate + "T00:00:00.000Z" + "/" + passengers
+                }).then(function callback(response) {
+                    dataContainer.set(response.data);
+                    $scope.results = dataContainer.get();
+                    $location.path("/results");
+                });
+            }
+
+
+        };
+    }]);
