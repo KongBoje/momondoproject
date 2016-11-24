6f891beaf9b353c164253b870ffdbd2ee24217ed
Michael Veilis <Michael Veilis>
2016-11-24 12:56:03 +0100

    har lavet flight entity klassens id om til en int fra en Long. Har lavet en JUnit test som kan få en flight

--- src/main/java/Entity/Flight.java
+++ src/main/java/Entity/Flight.java
@@ -26,7 +26,7 @@ public class Flight implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String flightId;
     private String flightNumber;
     private String date;
@@ -127,11 +127,11 @@ public class Flight implements Serializable {
 
     
     
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
--- src/test/java/JUnit/Tests.java
+++ src/test/java/JUnit/Tests.java
@@ -6,6 +6,8 @@
 package JUnit;
 
 import Entity.Flight;
+import Facade.FlightFacade;
+import Interface.IFlightFacade;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -16,33 +18,45 @@ import org.junit.Before;
 
 public class Tests {
 
-//    private EntityManagerFactory entityManagerFactory;
-//
-//    @Before
-//    public void init() {
-//        entityManagerFactory = Persistence.createEntityManagerFactory("mmp");
-//    }
-//
-//    @After
-//    public void destroy() {
-//        entityManagerFactory.close();
-//    }
-//
-//    @Test
-//    public void connection() {
-//        EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        entityManager.getTransaction().begin();
-//
-//        Flight flight = new Flight();
-//        entityManager.persist(flight);
-//
-//        Flight dbFlight = entityManager.createQuery(
-//                "select e "
-//                + "from Flight e", Flight.class)
-//                .getSingleResult();
-//        assertEquals(flight.getFlightId(), dbFlight.getFlightId());
-//
-//        entityManager.getTransaction().commit();
-//        entityManager.close();
-//    }
+    private EntityManagerFactory entityManagerFactory;
+
+    @Before
+    public void init() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("mmp");
+    }
+
+    @After
+    public void destroy() {
+        entityManagerFactory.close();
+    }
+
+    @Test
+    public void connection() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Flight flight = new Flight();
+        entityManager.persist(flight);
+
+        Flight dbFlight = entityManager.createQuery(
+                "select e "
+                + "from Flight e", Flight.class)
+                .getSingleResult();
+        assertEquals(flight.getFlightId(), dbFlight.getFlightId());
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+    
+    @Test
+    public void testGetFlight(){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        FlightFacade fc = new FlightFacade();
+        Flight flight = new Flight();
+        fc.addFlight(flight);
+        assertTrue(fc.getFlight(flight.getId()).equals(flight));
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 }
