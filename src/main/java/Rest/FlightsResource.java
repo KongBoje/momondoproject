a22d59e3e83ca00d263392eeff0855884b3da790
Lasse Hansen <Lasse Hansen>
2016-11-28 11:17:06 +0100

    Readded get methods to the flights resource

--- src/main/java/Rest/FlightsResource.java
+++ src/main/java/Rest/FlightsResource.java
@@ -5,6 +5,10 @@
  */
 package Rest;
 
+import Entity.Airline;
+import Entity.Flight;
+import com.google.gson.Gson;
+import java.util.ArrayList;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -12,6 +16,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -21,6 +26,18 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("flights")
 public class FlightsResource {
+    
+    private static final Gson gson = new Gson();
+
+    ArrayList<Flight> flightList = new ArrayList<Flight>() {
+        {
+            add(new Flight("5342-5374581236347", "KAF6666", "2016-11-22T00:00:00.000Z", 6, 67, 240, "CPH", "SXF"));
+            add(new Flight("4443-8765989054363", "KAF3466", "2016-12-03T00:00:00.000Z", 9, 239, 120, "CPH", "STN"));
+            add(new Flight("2323-8675048485030", "KAF6622", "2016-03-12T00:00:00.000Z", 4, 190, 60, "STN", "SXF"));
+            add(new Flight("8765-0101909245668", "KAF5789", "2017-01-05T00:00:00.000Z", 12, 300, 264, "STN", "CPH"));
+            add(new Flight("6464-0192837465951", "KAF1113", "2017-02-01T00:00:00.000Z", 14, 200, 210, "SXF", "CPH"));
+        }
+    };
 
     @Context
     private UriInfo context;
@@ -35,19 +52,35 @@ public class FlightsResource {
      * Retrieves representation of an instance of Rest.FlightsResource
      * @return an instance of java.lang.String
      */
-    @GET
+    @GET // Fetches available flights from a specific location, given a date
+    @Path("/{from}/{date}/{tickets}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    public String fromDate(@PathParam("from") String from, @PathParam("date") String date, @PathParam("tickets") int tickets) {
+        Airline airline = new Airline("Kaffemænd Tours");
+        for(Flight x : flightList) {
+            if(x.getDate().equals(date) && x.getOrigin().equals(from)) {
+                airline.addFlight(x);
+            }
+        }
+        String res = gson.toJson(airline);
+        return res;
     }
 
     /**
      * PUT method for updating or creating an instance of FlightsResource
      * @param content representation for the resource
      */
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    @GET // Fetches available flights from and to a specific location, given a date 
+    @Path("/{from}/{to}/{date}/{tickets}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String fromToDateTick(@PathParam("from") String from, @PathParam("to") String to, @PathParam("date") String date, @PathParam("tickets") int tickets) {
+        Airline airline = new Airline("Kaffemænd Tours");
+        for(Flight x : flightList) {
+            if(x.getDate().equals(date) && x.getOrigin().equals(from) && x.getDestination().equals(to)) {
+                airline.addFlight(x);
+            }
+        }
+        String res = gson.toJson(airline);
+        return res;
     }
 }
