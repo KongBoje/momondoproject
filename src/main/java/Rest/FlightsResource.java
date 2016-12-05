ba3f78a16009663a9a3164bfc4657e1194210340
Lasse Hansen <Lasse Hansen>
2016-12-05 12:47:40 +0100

    Preparation to having the source from get search results

--- src/main/java/Rest/FlightsResource.java
+++ src/main/java/Rest/FlightsResource.java
@@ -9,15 +9,14 @@ import Entity.Airline;
 import Entity.Flight;
 import Exceptions.FlightException;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
